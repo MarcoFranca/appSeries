aa9837b4210b9011f245e7f911513016637f4ad8
Marco Franca <dev.marcotullio@gmail.com>
2022-12-27 08:16:38 -0300

    estilização do primeiro e ultimo imput [UPDATE]

--- src/components/FormRow.js
+++ src/components/FormRow.js
@@ -3,9 +3,11 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 
 const FormRow = (props) => {
-    const {children} = props
+    const {children, last, first} = props
     return (
-        <View style={styles.container}>
+        <View style={[styles.container,
+            first? styles.first: null ,
+            last? styles.last: null]}>
             {children}
         </View>
     );
@@ -14,12 +16,18 @@ const FormRow = (props) => {
 const styles = StyleSheet.create({
     container:{
         padding:10,
-        backgroundColor:'#FFFFFF',
+        backgroundColor:'rgb(255,255,255)',
         marginTop:5,
         marginBottom:5,
         elevation:1,
-
-    }
+        borderRadius:10
+    },
+    first:{
+        marginTop:10,
+    },
+    last:{
+        marginBottom:10,
+    },
 })
 export default FormRow;
 
